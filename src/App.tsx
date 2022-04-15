534b56537389900d8225aa51b1ec7d2c79b3caf5
zepedroc <zepedrocm@hotmail.com>
2022-04-15 20:53:30 +0100

    Connect Fibonacci API

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const FIBONACCI_API = 'https://wondrous-dango-ab8623.netlify.app/.netlify/functions/api';
+
 function App() {
+  const [fibNumber, setFibNumber] = useState(0);
+  const [position, setPosition] = useState(0);
+  const [requestsExecuted, setRequestsExecuted] = useState([]);
+
+  // "proxy": "https://wondrous-dango-ab8623.netlify.app/.netlify/functions/api",
+
+  useEffect(() => {
+    fetch(`${FIBONACCI_API}/requests`)
+      .then(res => res.json())
+      .then(data => {
+        setRequestsExecuted(data['requests']);
+      });
+  }, []);
+
+  const getFibNumber = () => {
+    fetch(`${FIBONACCI_API}/fibSeqNum/${position}`)
+      .then(res => res.json())
+      .then(data => {
+        setFibNumber(data['fibNum']);
+
+        // update requests data
+        fetch(`${FIBONACCI_API}/requests`)
+          .then(res => res.json())
+          .then(data => {
+            setRequestsExecuted(data['requests']);
+          });
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      What Fibonacci Sequence number would you like to request?
+      <input type="number" min="1" onChange={(ev) => setPosition(parseInt(ev.target.value))} />
+      <button onClick={getFibNumber}>Get</button>
+
+      <div>Tadaaaaaa: {fibNumber}</div>
+
+      <h2>Previous Requests</h2>
+      <div>{requestsExecuted.map(req => (
+        <div>
+          <div>
+            Position Requested: {req['positionRequested']}
+          </div>
+          <div>
+            Result: {req['result']}
+          </div>
+          <div>
+            Date: {new Date(req['date']).toString()}
+          </div>
+        </div>
+      ))}</div>
     </div>
   );
 }
